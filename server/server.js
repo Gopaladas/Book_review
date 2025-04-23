bbfe93ae19dfeae1d247fdd89843e1a7be83a776
Pavankumar <114066364+Gopaladas@users.noreply.github.com>
2025-04-23 15:14:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://book-review-eight-sand.vercel.app",
     credentials: true,
   })
 );
