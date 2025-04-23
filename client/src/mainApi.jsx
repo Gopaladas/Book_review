9ca9b248781643994b7e9201f7e9eb14e19bff1f
Pavankumar <114066364+Gopaladas@users.noreply.github.com>
2025-04-23 15:23:39 +0530

    Update mainApi.jsx

--- client/src/mainApi.jsx
+++ client/src/mainApi.jsx
@@ -1,7 +1,7 @@
-const backendApi = "http://localhost:4000/api";
+const backendApi = "https://book-review-lj7c.vercel.app/api";
 const userApi = `${backendApi}/user`;
 const adminApi = `${backendApi}/admin`;
 const authApi = `${backendApi}/auth`;
-const serverApi="http://localhost:4000/";
+const serverApi="https://book-review-lj7c.vercel.app/";
 
 export { backendApi,userApi,adminApi,authApi,serverApi };
