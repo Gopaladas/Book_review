bfcd5d035c1ba1f1a4355d6df6ab5919e5de346a
Pavankumar <114066364+Gopaladas@users.noreply.github.com>
2025-04-23 16:07:33 +0530

    Update mainApi.jsx

--- client/src/mainApi.jsx
+++ client/src/mainApi.jsx
@@ -1,7 +1,7 @@
-const backendApi = "https://book-review-lj7c.vercel.app/api";
+const backendApi = "https://book-review-rifj.onrender.com/api";
 const userApi = `${backendApi}/user`;
 const adminApi = `${backendApi}/admin`;
 const authApi = `${backendApi}/auth`;
-const serverApi="https://book-review-lj7c.vercel.app/";
+const serverApi="https://book-review-rifj.onrender.com/";
 
 export { backendApi,userApi,adminApi,authApi,serverApi };
